623e3af5149e43b0b9f757b2d6e3df8d4214fc88
munirmurad2165 <murad2165@outlook.com>
2016-04-29 00:13:59 -0700

    Updating wk1 files

--- wk1/testw1p10.py
+++ wk1/testw1p10.py
@@ -1,12 +1,22 @@
 from ciscoconfparse import CiscoConfParse
+import re
 
-cisco_cfg=CiscoConfParse("cisco_ipsec.txt")
 
-crypto_map=cisco_cfg.find_objects_wo_child(parentspec=r'crypto map CRYPTO', childspec=r'AES')
+def main():
 
-for child in crypto_map:
-    print child.text
-    for ch in child.children:
-        print ch.text
+    cisco_cfg=CiscoConfParse("cisco_ipsec.txt")
+
+    crypto_map=cisco_cfg.find_objects_wo_child(parentspec=r'crypto map CRYPTO', childspec=r'AES')
+
+    for child in crypto_map:
+        #print child.text
+        for ch in child.children:
+            if 'transform-set' in ch.text:
+                match=re.search(r'set transform-set (.*)',ch.text)
+                result=match.group(1)
+                print "{0}>>> {1}".format(child.text,result)
+
+if __name__ == "__main__":
+    main()
 
 
--- wk1/testw1p9.py
+++ wk1/testw1p9.py
@@ -1,12 +1,19 @@
 from ciscoconfparse import CiscoConfParse
 
-cisco_cfg=CiscoConfParse("cisco_ipsec.txt")
 
-crypto_map=cisco_cfg.find_objects_w_child(parentspec=r'crypto map CRYPTO', childspec=r'pfs group2')
+def main():
 
-for child in crypto_map:
-    print child.text
-    for ch in child.children:
-        print ch.text
+    cisco_cfg=CiscoConfParse("cisco_ipsec.txt")
+
+    crypto_map=cisco_cfg.find_objects_w_child(parentspec=r'crypto map CRYPTO', childspec=r'pfs group2')
+
+    print "Crypto Map Using PFS Group2: "
+
+    for child in crypto_map:
+        print "{0}".format(child.text)
+    #for ch in child.children:
+     #   print ch.itext
+if __name__=="__main__":
+    main()
 
 
